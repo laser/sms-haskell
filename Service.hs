28de64988076bc085568745224184520d1c37218
Erin Swenson-Healey <l@s3r.me>
2015-08-14 07:59:56 -0700

    Update user login info on auth with Google

---
+++ Service.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Service (login) where
+
+import Data.Int (Int64)
+import Control.Error (syncIO, bimapExceptT)
+import Control.Monad.Trans.Except (ExceptT)
+import Control.Exception (SomeException)
+import Database.MySQL.Simple (ConnectInfo(..), Connection, execute, connect, close, defaultConnectInfo)
+
+dbConfig :: ConnectInfo
+dbConfig = defaultConnectInfo { connectDatabase = "sms" }
+
+withConnection :: (Connection -> IO a) -> IO a
+withConnection f = do
+  con <- connect dbConfig
+  res <- f con
+  close con
+  return res
+
+login :: String -> String -> String -> String -> ExceptT SomeException IO String
+login token userId email name =
+  upsertUser userId email name
+  >> recordLogin token userId
+  >> linkProjectAccess userId email
+  >> return token
+
+upsertUser :: String -> String -> String -> ExceptT SomeException IO Int64
+upsertUser userId email name = syncIO . withConnection $ \conn -> 
+  execute conn 
+    " INSERT INTO `users` (user_id, email, name, date_created) \
+    \ VALUES (?, ?, ?, NOW()) \
+    \ ON DUPLICATE KEY UPDATE email=(?), name=(?) "
+    (userId, email, name, email, name)
+
+linkProjectAccess :: String -> String -> ExceptT SomeException IO Int64
+linkProjectAccess userId email = syncIO . withConnection $ \conn -> 
+  execute conn 
+    " UPDATE `project_access` \
+    \ SET user_id = (?) \
+    \ WHERE email=(?) "
+    (userId, email)
+
+recordLogin :: String -> String -> ExceptT SomeException IO Int64
+recordLogin token userId = syncIO . withConnection $ \conn -> 
+  execute conn
+    " INSERT INTO `logins` (access_token, user_id, expiry_time) \
+    \ VALUES (?, ?, NOW()+INTERVAL 1 DAY) \
+    \ ON DUPLICATE KEY UPDATE access_token=(?), expiry_time=NOW()+INTERVAL 1 DAY "
+    (token, userId, token)
