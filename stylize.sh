c639525177b68d6cde3d695db1787e1e4b390a20
Erin Swenson-Healey <l@s3r.me>
2015-08-18 12:37:28 -0700

    stylish-haskell to project packages

--- stylize.sh
+++ stylize.sh
@@ -1,1 +1,1 @@
-find . -name "*.hs" -exec sh -c '{ rm "$0" && stylish-haskell > "$0"; } < "$0"' {} \;
+find . -name "*.hs" -exec sh -c '{ rm "$0" && /app/sandbox/bin/stylish-haskell > "$0"; } < "$0"' {} \;
